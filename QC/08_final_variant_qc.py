72e62d10f93fe4eba5576fde5cfdd6055d7e8227
astheeggeggs <youremail@email.com>
2022-05-25 17:26:29 +0100

    added a few more function to BRaVa curation

---
+++ QC/08_final_variant_qc.py
@@ -0,0 +1,85 @@
+import hail as hl
+import sys
+
+hl.init()
+
+# Inputs
+MT = sys.argv[1]
+IMPUTESEX_TABLE = sys.argv[2]
+SUPERPOPS = sys.argv[3]
+SEXCHECK_LIST = sys.argv[4]
+RELATED_SAMPLES = sys.argv[5]
+INITIAL_VARIANT_LIST = sys.argv[6]
+SAMPLE_LIST_INITIAL_QC = sys.argv[7]
+REFERENCE = 'GRCh38'
+
+# Outputs
+VARIANT_QC_FILE = sys.argv[8]
+
+# # Inputs:
+# TRANCHE = '200k'
+# CHR = '20'
+# MT = '/well/lindgren/UKBIOBANK/dpalmer/wes_' + TRANCHE + '/ukb_wes_qc/data/filtered/ukb_wes_' + TRANCHE + '_filtered_hardcalls_chr' + CHR + '.mt'
+# IMPUTESEX_TABLE = '/well/lindgren/UKBIOBANK/dpalmer/wes_' + TRANCHE + '/ukb_wes_qc/data/samples/04_imputesex_'
+# SUPERPOPS = "/well/lindgren/UKBIOBANK/dpalmer/superpopulation_assignments/superpopulation_labels.tsv"
+# SEXCHECK_LIST = '/well/lindgren/UKBIOBANK/dpalmer/wes_' + TRANCHE + '/ukb_wes_qc/data/samples/04_sexcheck.remove.sample_list'
+# RELATED_SAMPLES = '/well/lindgren/UKBIOBANK/dpalmer/wes_200k/ukb_wes_qc/data/samples/06_king.related.sample_list'
+# INITIAL_VARIANT_LIST = '/well/lindgren/UKBIOBANK/dpalmer/wes_' + TRANCHE + '/ukb_wes_qc/data/variants/02_prefilter_chr' + CHR + '.keep.variant.ht'
+# SAMPLE_LIST_INITIAL_QC = '/well/lindgren/UKBIOBANK/dpalmer/wes_' + TRANCHE + '/ukb_wes_qc/data/samples/03_initial_qc.keep.sample_list'
+
+# # Outputs:
+# VARIANT_QC_FILE = '/well/lindgren/UKBIOBANK/dpalmer/wes_' + TRANCHE + '/ukb_wes_qc/data/variants/08_final_qc.variants_chr' + CHR + '_'
+
+print("Inputs:")
+print('MT; input matrix table: ', MT)
+print('IMPUTESEX_TABLE; input file prefix of .tsvs file to plot imputed sex information output from 06_0_impute_sex_superpopulations.py: ', IMPUTESEX_TABLE)
+print('SUPERPOPS; list of 1000G labels to loop over:', SUPERPOPS)
+print('SEXCHECK_LIST; list of sex swaps to remove:', SEXCHECK_LIST)
+print('RELATED_SAMPLES; input set of related samples output from 07_0_ibd.py, 07_0_pc_relate.py, or 07_0_ukb_relatedness_king.py', RELATED_SAMPLES)
+print('INITIAL_VARIANT_LIST; set of variants that pass initial filtering \
+	(output from 02_prefilter_variants.py): ', INITIAL_VARIANT_LIST)
+print('SAMPLE_LIST_INITIAL_QC; set of initial samples output from 03_01_initial_sample_qc_filter.r: ', SAMPLE_LIST_INITIAL_QC)
+
+print("Outputs:")
+print('VARIANT_QC_FILE; output .tsv file variant QC information: ', VARIANT_QC_FILE)
+
+ht_superpops = hl.import_table(SUPERPOPS, impute=True).key_by("sample.ID").select("classification_strict")
+ht_related_samples = hl.import_table(RELATED_SAMPLES, no_header=True, key='f0')
+ht_initial_variants = hl.read_table(INITIAL_VARIANT_LIST)
+ht_sexcheck_samples = hl.import_table(SEXCHECK_LIST, no_header=True, key='f0')
+ht_initial_samples = hl.import_table(SAMPLE_LIST_INITIAL_QC, no_header=True, key='f0')
+
+mt = hl.read_matrix_table(MT)
+mt = mt.annotate_cols(**ht_superpops[mt.s])
+mt = mt.filter_rows(hl.is_defined(ht_initial_variants[mt.row_key]))
+mt = mt.filter_cols(hl.is_defined(ht_initial_samples[mt.col_key]))
+mt = mt.filter_cols(~hl.is_defined(ht_sexcheck_samples[mt.col_key]))
+mt = mt.filter_cols(~hl.is_defined(ht_related_samples[mt.col_key]))
+
+for pop in ["AFR", "AMR", "EAS", "EUR", "SAS"]:
+	impute_sex_annotations = hl.read_table(IMPUTESEX_TABLE + pop + '.ht')
+	mt_pop = mt.filter_cols(mt.classification_strict == pop)
+	mt_pop = mt.annotate_cols(imputesex = impute_sex_annotations[mt.col_key])
+	mt_pop = hl.variant_qc(mt_pop, name='variant_qc')
+
+	mt_pop = mt_pop.annotate_rows(
+		variant_qc=mt_pop.variant_qc.annotate(AC=mt_pop.variant_qc.AC[1],
+		AF=mt_pop.variant_qc.AF[1],
+		homozygote_count=mt_pop.variant_qc.homozygote_count[1]))
+
+	mt_pop = mt_pop.annotate_rows(
+		variant_qc = mt_pop.variant_qc.annotate(p_value_hwe = hl.case()
+		.when(mt_pop.locus.in_autosome(), mt_pop.variant_qc.p_value_hwe)
+		.default(hl.agg.filter(mt_pop.imputesex.impute_sex.is_female,
+			hl.agg.hardy_weinberg_test(mt_pop.GT).p_value)))
+	)
+
+	mt_pop = mt_pop.annotate_rows(
+		variant_qc = mt_pop.variant_qc.annotate(het_freq_hwe = hl.case()
+		.when(mt_pop.locus.in_autosome(), mt_pop.variant_qc.het_freq_hwe)
+		.default(hl.agg.filter(mt_pop.imputesex.impute_sex.is_female,
+			hl.agg.hardy_weinberg_test(mt_pop.GT).het_freq_hwe)))
+	)
+
+	mt_pop.rows().select('variant_qc').flatten().export(f'{VARIANT_QC_FILE}{pop}.tsv.bgz')
+
---
+++ QC/10_check_sample_variant_metrics_plot.r
@@ -0,0 +1,185 @@
+library(ggplot2)
+library(ggsci)
+library(data.table)
+
+# Plotting functions:
+source('utils/pretty_plotting.r')
+# Thresholds and plotting file locations defined in r_options.r
+source("utils/r_options.r")
+
+suppressPackageStartupMessages(library("argparse"))
+
+CHR <- 1
+TRANCHE <- '200k'
+save_figures <- TRUE
+PLOTS <- '/well/lindgren/dpalmer/ukb_exome_qc/plots/'
+
+# Input files
+SAMPLE_QC_FILE <- paste0("/well/lindgren/UKBIOBANK/dpalmer/wes_", TRANCHE, "/ukb_wes_qc/data/samples/10_sample_metrics_for_plotting_chr", CHR, ".tsv.gz")
+VARIANT_QC_FILE <- paste0("/well/lindgren/UKBIOBANK/dpalmer/wes_", TRANCHE, "/ukb_wes_qc/data/variants/10_variant_metrics_for_plotting_chr", CHR, ".tsv.gz")
+
+# Output files
+COMBINED_SAMPLE_QC_FILE <- paste0('/well/lindgren/UKBIOBANK/dpalmer/wes_', TRANCHE, '/ukb_wes_qc/data/10_sample_metrics_for_plotting.tsv')
+COMBINED_VARIANT_QC_FILE <- paste0('/well/lindgren/UKBIOBANK/dpalmer/wes_', TRANCHE, '/ukb_wes_qc/data/10_variant_metrics_for_plotting.tsv')
+
+dt <- fread(cmd = paste('zcat', VARIANT_QC_FILE), header=TRUE, sep='\t')
+
+dt_list <- list()
+dt_list[[1]] <- dt
+
+for (CHR in c(seq(2,22), "X")) {
+    # Input files
+    cat(paste0("chromosome ", CHR, "\n"))
+    VARIANT_QC_FILE <- paste0("/well/lindgren/UKBIOBANK/dpalmer/wes_", TRANCHE, "/ukb_wes_qc/data/variants/10_variant_metrics_for_plotting_chr", CHR, ".tsv.gz")
+    dt_tmp <- fread(cmd = paste('zcat', VARIANT_QC_FILE), header=TRUE, sep='\t')
+    setkeyv(dt_tmp, c('locus', 'alleles'))
+    dt_list[[CHR]] <- dt_tmp
+}
+
+dt <- rbindlist(dt_list)
+
+fwrite(dt, file=COMBINED_VARIANT_QC_FILE , sep='\t', quote=FALSE)
+
+dt <- fread(COMBINED_VARIANT_QC_FILE)
+
+# call rate across all variants
+create_pretty_hist(dt, aes(x=variant_qc.call_rate), x_label='Call Rate', save_figure=save_figures,
+	file=paste0(PLOTS, TRANCHE, '_10_callRate_hist'))
+# cumulative call rate
+create_pretty_cumulative(dt, aes(x=variant_qc.call_rate), x_label="Call Rate", threshold=NULL,
+    key_label='', save_figure=save_figures, file=paste0(PLOTS, TRANCHE, '_10_callRate_cdf'))
+
+# mean DP all variants
+create_pretty_hist(dt, aes(x=variant_qc.dp_stats.mean), x_label='Mean DP', save_figure=save_figures,
+    file=paste0(PLOTS, TRANCHE, '_10_mean_DP_hist'))
+# cumulative mean DP
+create_pretty_cumulative(dt, aes(x=variant_qc.dp_stats.mean), x_label="Mean DP", threshold=NULL,
+    key_label='', save_figure=save_figures, file=paste0(PLOTS, TRANCHE, '_10_mean_DP_cdf'))
+
+# mean GQ all variants
+create_pretty_hist(dt, aes(x=variant_qc.gq_stats.mean), x_label='Mean GQ', save_figure=save_figures,
+    file=paste0(PLOTS, TRANCHE, '_10_mean_GQ_hist'))
+# cumulative mean GQ
+create_pretty_cumulative(dt, aes(x=variant_qc.call_rate), x_label='Mean GQ', threshold=NULL,
+    key_label='', save_figure=save_figures, file=paste0(PLOTS, TRANCHE, '_10_mean_GQ_cdf'))
+
+# pHWE across all variants
+p <- create_pretty_hist(dt, aes(x=variant_qc.p_value_hwe), x_label='p(HWE)',
+    xlim=c(NA,1), save_figure=FALSE) + scale_x_log10(
+    breaks = scales::trans_breaks("log10", function(x) 10^x),
+    labels = scales::trans_format("log10", scales::math_format(10^.x))
+  )
+ggsave(paste0(PLOTS, TRANCHE, '_10_pHWE_hist.pdf'), p, width=160, height=90, units='mm')
+ggsave(paste0(PLOTS, TRANCHE, '_10_pHWE_hist.jpg'), p, width=160, height=90, units='mm')
+
+# cumulative pHWE
+p <- create_pretty_cumulative(dt, aes(x=variant_qc.p_value_hwe), x_label='p(HWE)', threshold=NULL,
+    xlim=c(NA,1), save_figure=FALSE) #+ scale_x_log10(
+    # breaks = scales::trans_breaks("log10", function(x) 10^x),
+    # labels = scales::trans_format("log10", scales::math_format(10^.x))
+  # )
+ggsave(paste0(PLOTS, TRANCHE, '_10_pHWE_cdf.pdf'), p, width=160, height=90, units='mm')
+ggsave(paste0(PLOTS, TRANCHE, '_10_pHWE_cdf.jpg'), p, width=160, height=90, units='mm')
+
+# What other metrics are there?
+# Can I also export these other summary metrics?
+
+
+# For samples
+
+read_and_key <- function(gz_filepath, starts='sample_qc', tmp=FALSE) {
+    dt <- fread(cmd = paste('zcat', gz_filepath),
+        stringsAsFactors=FALSE, sep='\t', header=TRUE) %>% select(s, starts_with(starts))
+    if (tmp) {
+        names(dt) <- c('s', paste0('tmp_', names(dt)[-1]))
+    }
+    setkey(dt, 's')
+    return(dt)
+}
+
+update_entries <- function(dt) {
+    dt <- dt %>% mutate(
+        # Counts
+        sample_qc.n_called = sample_qc.n_called + tmp_sample_qc.n_called,
+        sample_qc.n_not_called = sample_qc.n_not_called + tmp_sample_qc.n_not_called,
+        sample_qc.n_filtered = sample_qc.n_filtered + tmp_sample_qc.n_filtered,
+        sample_qc.n_hom_ref = sample_qc.n_hom_ref + tmp_sample_qc.n_hom_ref,
+        sample_qc.n_het = sample_qc.n_het + tmp_sample_qc.n_het,
+        sample_qc.n_hom_var = sample_qc.n_hom_var + tmp_sample_qc.n_hom_var,
+        sample_qc.n_non_ref = sample_qc.n_non_ref + tmp_sample_qc.n_non_ref,
+        sample_qc.n_singleton = sample_qc.n_singleton + tmp_sample_qc.n_singleton,
+        sample_qc.n_snp = sample_qc.n_snp + tmp_sample_qc.n_snp,
+        sample_qc.n_insertion = sample_qc.n_insertion + tmp_sample_qc.n_insertion,
+        sample_qc.n_deletion = sample_qc.n_deletion + tmp_sample_qc.n_deletion,
+        sample_qc.n_transition = sample_qc.n_transition + tmp_sample_qc.n_transition,
+        sample_qc.n_transversion = sample_qc.n_transversion + tmp_sample_qc.n_transversion,
+        sample_qc.n_star = sample_qc.n_star + tmp_sample_qc.n_star
+        ) %>% select(c(s, starts_with('sample_qc')))
+    setkey(dt, 's')
+    return(dt)
+}
+
+add_ratios <- function(dt) {
+    dt <- dt %>% mutate(
+        # Ratios
+        sample_qc.call_rate = sample_qc.n_called / (sample_qc.n_called + sample_qc.n_not_called + sample_qc.n_filtered),
+        sample_qc.r_ti_tv = ifelse(sample_qc.n_transversion == 0, NA, sample_qc.n_transition / sample_qc.n_transversion),
+        sample_qc.r_het_hom_var = ifelse(sample_qc.n_hom_var == 0, NA, sample_qc.n_het / sample_qc.n_hom_var),
+        sample_qc.r_insertion_deletion = ifelse(sample_qc.n_deletion == 0, NA, sample_qc.n_insertion / sample_qc.n_deletion),
+    ) %>% select(c('s', starts_with('sample_qc')))
+    setkey(dt, 's')
+    return(dt)
+}
+
+dt <- read_and_key(SAMPLE_QC_FILE)
+
+for (CHR in c(seq(2,22), "X")) {
+    # Input files
+    cat(paste0("chromosome ", CHR, "\n"))
+    SAMPLE_QC_FILE <- paste0(
+        "/well/lindgren/UKBIOBANK/dpalmer/wes_", TRANCHE,
+        "/ukb_wes_qc/data/samples/10_sample_metrics_for_plotting_chr", CHR, ".tsv.gz")
+
+    dt_tmp <- read_and_key(SAMPLE_QC_FILE, tmp=TRUE)
+    dt <- merge(dt, dt_tmp)
+    dt <- update_entries(dt)
+}
+
+dt <- add_ratios(dt)
+
+# Could include boxplots - I think here I can just include batch.
+pdf("test.pdf")
+p <- ggplot(dt, aes(y=sample_qc.r_ti_tv)) + geom_boxplot() + theme_classic()
+print(p)
+dev.off()
+
+# rTiTv
+create_pretty_hist(dt, aes(x=sample_qc.r_ti_tv), x_label='Transition/Transversion', save_figure=save_figures,
+    file=paste0(PLOTS, TRANCHE, '_10_ti_tv_hist'))
+
+# rHetHomVar
+create_pretty_hist(dt, aes(x=sample_qc.r_het_hom_var), x_label='(Het)/(Hom var)', save_figure=save_figures,
+    file=paste0(PLOTS, TRANCHE, '_10_het_hom_var_hist'))
+
+# rInsertionDeletion
+create_pretty_hist(dt, aes(x=sample_qc.r_insertion_deletion), x_label='Insertion/Deletion', save_figure=save_figures,
+    file=paste0(PLOTS, TRANCHE, '_10_insertion_deletion'))
+
+# Number of singletons
+create_pretty_hist(dt, aes(x=sample_qc.n_singleton), x_label='Number of singletons', save_figure=save_figures,
+    file=paste0(PLOTS, TRANCHE, '_10_n_singleton_hist'))
+
+# Call rate across all samples
+create_pretty_hist(dt, aes(x=sample_qc.call_rate), x_label='Call Rate', save_figure=save_figures,
+    file=paste0(PLOTS, TRANCHE, '_10_sample_callRate_hist'))
+
+# mean DP all variants
+create_pretty_hist(dt, aes(x=sample_qc.dp_stats.mean), x_label='Mean DP', save_figure=save_figures,
+    file=paste0(PLOTS, TRANCHE, '_10_sample_mean_DP_hist'))
+
+# mean GQ all variants
+create_pretty_hist(dt, aes(x=sample_qc.gq_stats.mean), x_label='Mean GQ', save_figure=save_figures,
+    file=paste0(PLOTS, TRANCHE, '_10_sample_mean_GQ_hist'))
+
+# Want to store super straightforward metrics for the samples too.
+

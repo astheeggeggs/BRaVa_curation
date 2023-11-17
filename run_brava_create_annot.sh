68395c6aa7d9484c849a909d29a8ba3a327ac883
astheeggeggs <duncan.stuart.palmer@gmail.com>
2023-11-17 11:10:50 +0000

    add submission script to generate annotations - rerunning this to create long format annotations for downstream plotting

--- run_brava_create_annot.sh
+++ run_brava_create_annot.sh
@@ -20,6 +20,7 @@ spliceai_dir="/well/lindgren/barney/spliceai/out"
 vep_annotations_dir="/well/lindgren/barney/variant-annotation/run-vep/out"
 out_dir="/well/lindgren/UKBIOBANK/dpalmer/annotation_munging"
 annotation_repo_dir="/well/lindgren/UKBIOBANK/dpalmer/variant-annotation"
+brava_create_annot="${annotation_repo_dir}/SAIGE_annotations/scripts/brava_create_annot.py"
 mkdir -p ${out_dir}
 
 for chr in {{1..22},"X"}
@@ -27,5 +28,5 @@ do
     vep="${vep_annotations_dir}/ukb_wes_450k.qced.chr${chr}.vep_processed.txt"
     spliceai="${spliceai_dir}/ukb_wes_450k.qced.v6.sites_only.${chr}.all.vcf"
     out="${out_dir}/ukb_wes_450k.july.qced.brava_common_rare.v7.chr${chr}.saige.txt"
-    python3 ${annotation_repo_dir}/brava_create_annot.py --vep ${vep} --spliceai ${spliceai} --out_file $out
+    python3 ${brava_create_annot} --vep ${vep} --spliceai ${spliceai} --out_file $out
 done

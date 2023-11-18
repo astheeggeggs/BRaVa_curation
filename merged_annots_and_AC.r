df190ab07ab5161ff860b645d2065061d7c7b083
astheeggeggs <duncan.stuart.palmer@gmail.com>
2023-11-18 13:26:37 +0000

    updated spliceAI annotations - removing chr to match the VEP annotations

--- merged_annots_and_AC.r
+++ merged_annots_and_AC.r
@@ -4,24 +4,27 @@ library(argparse)
 library(dplyr)
 
 CSQ_CODING_HIGH_IMPACT <- c(
-    "transcript_ablation", "splice_acceptor_variant", "splice_donor_variant", "stop_gained", 
-    "frameshift_variant", "stop_lost")
+    "transcript_ablation", "splice_acceptor_variant", "splice_donor_variant",
+    "stop_gained", "frameshift_variant", "stop_lost")
 
 CSQ_CODING_MEDIUM_IMPACT <- c(
-    "start_lost", "initiator_codon_variant", "transcript_amplification", "inframe_insertion",
-    "inframe_deletion", "missense_variant", "protein_altering_variant", "splice_region_variant")
+    "start_lost", "initiator_codon_variant", "transcript_amplification",
+    "inframe_insertion", "inframe_deletion", "missense_variant",
+    "protein_altering_variant", "splice_region_variant")
 
 CSQ_CODING_LOW_IMPACT <- c(
-    "incomplete_terminal_codon_variant", "start_retained_variant", "stop_retained_variant",
-    "synonymous_variant", "coding_sequence_variant")
+    "incomplete_terminal_codon_variant", "start_retained_variant",
+    "stop_retained_variant", "synonymous_variant", "coding_sequence_variant")
 
 CSQ_NON_CODING <- c(
     "mature_miRNA_variant", "5_prime_UTR_variant", "3_prime_UTR_variant",
-    "non_coding_transcript_exon_variant", "non_coding_exon_variant", "intron_variant",
-    "NMD_transcript_variant", "non_coding_transcript_variant", "nc_transcript_variant",
-    "upstream_gene_variant", "downstream_gene_variant", "TFBS_ablation", "TFBS_amplification",
-    "TF_binding_site_variant", "regulatory_region_ablation", "regulatory_region_amplification",
-    "feature_elongation", "regulatory_region_variant", "feature_truncation", "intergenic_variant")
+    "non_coding_transcript_exon_variant", "non_coding_exon_variant",
+    "intron_variant", "NMD_transcript_variant", "non_coding_transcript_variant",
+    "nc_transcript_variant", "upstream_gene_variant", "downstream_gene_variant",
+    "TFBS_ablation", "TFBS_amplification", "TF_binding_site_variant",
+    "regulatory_region_ablation", "regulatory_region_amplification",
+    "feature_elongation", "regulatory_region_variant", "feature_truncation",
+    "intergenic_variant")
 
 # DEV: include the spliceAI annotation information in bins
 
@@ -30,8 +33,10 @@ main <- function(args)
     most_deleterious <- function(annotation) {
         case_when(
           "pLoF" %in% annotation ~ "pLoF",
-          "damaging_missense_or_protein_altering" %in% annotation ~ "damaging_missense_or_protein_altering",
-          "other_missense_or_protein_altering" %in% annotation ~ "other_missense_or_protein_altering",
+          "damaging_missense_or_protein_altering" %in% 
+          annotation ~ "damaging_missense_or_protein_altering",
+          "other_missense_or_protein_altering" %in% 
+          annotation ~ "other_missense_or_protein_altering",
           "synonymous" %in% annotation ~ "synonymous",
           .default = "non_coding"
         )
@@ -60,9 +65,35 @@ main <- function(args)
         )
     }
 
-    # Ensure that all variants above MAX_AF 1% in gnomAD have been removed - do it with and without this.
+    create_MAC_and_MAF_summary <- function(dt, annotation_group, fout)
+    {
+        annotation_group <- enquo(annotation_group)
+
+        dt_summary_MAF <- dt %>% group_by(!!annotation_group, MAF_bin) %>% 
+            summarise(average_AC = sum(MAC), variant_count = n())
+        dt_summary_MAC <- dt %>% group_by(!!annotation_group, MAC_bin) %>% 
+            summarise(average_AC = sum(MAC), variant_count = n())
+        dt_summary_MAF <- dt_summary_MAF %>% rename(bin=MAF_bin) %>% 
+            mutate(bin_type="MAF")
+        dt_summary_MAC <- dt_summary_MAC %>% rename(bin=MAC_bin) %>% 
+            mutate(bin_type="MAC")
+        dt_summary_MAF <- data.table(dt_summary_MAF)
+        dt_summary_MAC <- data.table(dt_summary_MAC)
+        dt_summary <- merge(dt_summary_MAF, dt_summary_MAC, all=TRUE)
+        dt_summary <- dt_summary %>% 
+            mutate(average_count = average_AC/n_samples)
+
+        fwrite(dt_summary, file=fout, sep="\t", quote=FALSE)
+        return(dt_summary)
+    }
+
+    # Ensure that all variants above MAX_AF 1% in gnomAD have been removed 
+    # do it with and without this.
     AC_path <- args$AC_path
-    # python brava_create_annot.py --vep ukb_wes_450k.qced.chr21.vep_processed.txt --spliceai ukb_wes_450k.qced.v6.sites_only.21.all.vcf --out_file test
+    # python brava_create_annot.py 
+    #       --vep ukb_wes_450k.qced.chr21.vep_processed.txt 
+    #       --spliceai ukb_wes_450k.qced.v6.sites_only.21.all.vcf 
+    #       --out_file test
     vep_spliceAI_path <- args$vep_spliceAI_processed
     
     dt_brava_annot <- fread(vep_spliceAI_path, key=c("SNP_ID"))
@@ -78,29 +109,36 @@ main <- function(args)
     dt_AC[, MAF := MAC/(2*n_samples)]
     dt_AC[, MAF_bin := cut(
         MAF,
-        breaks=c(0,(1/n_samples + 1/(2*n_samples)), 0.001, 0.01, 0.05, 0.5, 1),
-        labels=c("singletons", "<0.01%", "0.01-0.1%", "1-5%", ">5%", ">50%"),
+        breaks=c(0, 0.001, 0.01, 0.05, 0.5, 1),
+        labels=c("<0.01%", "0.01-0.1%", "1-5%", ">5%", ">50%"),
         include.lowest=TRUE)
     ]
+    dt_AC[, MAC_bin := cut(
+        MAC,
+        breaks=c(0, 1, 5, 10, 100, 1000, 10000, Inf),
+        labels=c("singletons", "(1,5]", "(5, 10]", "(10, 100]", "(100, 1,000]",
+            "(1,000, 10,000]", "> 10,000"))]
     dt_variant_gene_AC <- merge(dt_AC, dt_brava_annot)
     
-    # Transcript level - include variants multiple times where they appear on different transcripts
-    dt_summary <- dt_variant_gene_AC %>% group_by(annotation, MAF_bin) %>% summarise(average_AC = sum(MAC))
-    dt_summary <- dt_summary %>% mutate(average_count = average_AC/n_samples)
-    fwrite(dt_summary, file=paste0(args$out, ".BRaVa_annotations_transcript_summary.tsv.gz"), sep="\t", quote=FALSE)
+    # Transcript level
+    # Include variants multiple times where they appear on different transcripts
+    dt_summary <- create_MAC_and_MAF_summary(
+        dt_variant_gene_AC, annotation,
+        paste0(args$out, ".BRaVa_annotations_transcript_summary.tsv.gz"))
 
-    dt_variant_AC <- dt_variant_gene_AC %>% group_by(SNP_ID) %>% summarise(annotation = most_deleterious(annotation))
+    dt_variant_AC <- dt_variant_gene_AC %>% group_by(SNP_ID) %>% 
+        summarise(annotation = most_deleterious(annotation))
     dt_variant_AC <- data.table(dt_variant_AC, key="SNP_ID")
-    dt_variant_AC <- merge(dt_variant_AC, unique(dt_variant_gene_AC %>% select(c("SNP_ID","MAC", "MAF_bin"))))
+    dt_variant_AC <- merge(
+        dt_variant_AC,
+        unique(dt_variant_gene_AC %>% 
+            select(c("SNP_ID","MAC", "MAF_bin", "MAC_bin"))))
     
-    # Variant level - just include the most deleterious annotation for a variant
-    dt_summary <- dt_variant_AC %>% group_by(annotation, MAF_bin) %>% summarise(average_AC = sum(MAC))
-    dt_summary <- dt_summary %>% mutate(average_count = average_AC/n_samples)
-    fwrite(dt_summary, file=paste0(args$out, ".BRaVa_annotations_variant_summary.tsv.gz"), sep="\t", quote=FALSE)
-
-    # Create summary tables of the result and send to disk.
-    # Now do the same thing, but more fine grain, asking what is the distribution of counts of classes of variation output by VEP
-    # Use the gnomAD definitions for ordering of deleteriousness to define the class of variant that is annotated.
+    # Variant level
+    # Just include the most deleterious annotation for a variant
+    create_MAC_and_MAF_summary(
+        dt_variant_AC, annotation,
+        paste0(args$out, ".BRaVa_annotations_variant_summary.tsv.gz"))
 
     names_parse <- grep("&", names(table(dt_variant_gene_AC$CSQ)), value=TRUE)
     mapping <- list()
@@ -108,57 +146,50 @@ main <- function(args)
     mapping[names_no_parse] <- names_no_parse
     for (name in names_parse) {
         CSQs <- strsplit(name, split="&")[[1]]
-        mapping[[name]] <- case_when(
-            any(CSQs %in% CSQ_CODING_HIGH_IMPACT) ~ ifelse(
-                sum(CSQs %in% CSQ_CODING_HIGH_IMPACT) == 1,
-                CSQs[CSQs %in% CSQ_CODING_HIGH_IMPACT], 
-                paste(CSQs[CSQs %in% CSQ_CODING_HIGH_IMPACT], collapse="&")),
-            any(CSQs %in% CSQ_CODING_MEDIUM_IMPACT) ~ ifelse(
-                sum(CSQs %in% CSQ_CODING_MEDIUM_IMPACT) == 1,
-                CSQs[CSQs %in% CSQ_CODING_MEDIUM_IMPACT],
-                paste(CSQs[CSQs %in% CSQ_CODING_MEDIUM_IMPACT], collapse="&")),
-            any(CSQs %in% CSQ_CODING_LOW_IMPACT) ~ ifelse(
-                sum(CSQs %in% CSQ_CODING_LOW_IMPACT) == 1,
-                CSQs[CSQs %in% CSQ_CODING_LOW_IMPACT],
-                paste(CSQs[CSQs %in% CSQ_CODING_LOW_IMPACT], collapse="&")),
-            any(CSQs %in% CSQ_NON_CODING) ~ ifelse(
-                sum(CSQs %in% CSQ_NON_CODING) == 1,
-                CSQs[CSQs %in% CSQ_NON_CODING],
-                paste(CSQs[CSQs %in% CSQ_NON_CODING], collapse="&")),
-            .default = NA
-        )
+        mapping[[name]] <- most_deleterious_vep(CSQs)
     }
 
     # Place these within the CSQs column
     dt_variant_gene_AC[, CSQs := unlist(mapping[CSQ])]
 
-    # For the remainders, randomly assign them
+    # For the remainder, randomly assign them
     set.seed(42)
-    dt_variant_gene_AC[, CSQs := ifelse(grepl("&", CSQs), sample(strsplit(CSQs, split="&")[[1]], 1), CSQs)]
-
-    dt_summary <- dt_variant_gene_AC %>% group_by(CSQs, MAF_bin) %>% summarise(average_AC = sum(MAC))
-    dt_summary <- dt_summary %>% mutate(average_count = average_AC/n_samples)
-    fwrite(dt_summary, file=paste0(args$out, ".vep_annotations_transcript_summary.tsv.gz"), sep="\t", quote=FALSE)
-
-    dt_variant_AC <- dt_variant_gene_AC %>% group_by(SNP_ID) %>% summarise(CSQ = most_deleterious_vep(CSQs))
+    dt_variant_gene_AC[, CSQs := ifelse(
+        grepl("&", CSQs), sample(strsplit(CSQs, split="&")[[1]], 1), CSQs)]
+    
+    # Transcript level
+    # include variants multiple times where they appear on different transcripts
+    create_MAC_and_MAF_summary(
+        dt_variant_gene_AC, CSQs,
+        paste0(args$out, ".vep_annotations_transcript_summary.tsv.gz"))
 
     # Take the result and restrict to a single annotation per variant
+    dt_variant_AC <- dt_variant_gene_AC %>% group_by(SNP_ID) %>% 
+        summarise(CSQ = most_deleterious_vep(CSQs))
     dt_variant_AC <- data.table(dt_variant_AC, key="SNP_ID")
-    dt_variant_AC <- merge(dt_variant_AC, unique(dt_variant_gene_AC %>% select(c("SNP_ID","MAC", "MAF_bin"))))
-    dt_variant_AC[, CSQs := ifelse(grepl("&", CSQ), sample(strsplit(CSQ, split="&")[[1]], 1), CSQ)]
+    dt_variant_AC <- merge(
+        dt_variant_AC,
+        unique(dt_variant_gene_AC %>% 
+            select(c("SNP_ID","MAC", "MAF_bin", "MAC_bin"))))
+    dt_variant_AC[, CSQs := ifelse(
+        grepl("&", CSQ), sample(strsplit(CSQ, split="&")[[1]], 1), CSQ)]
 
-    # Variant level - just include the most deleterious annotation for a variant
-    dt_summary <- dt_variant_AC %>% group_by(CSQs, MAF_bin) %>% summarise(average_AC = sum(MAC))
-    dt_summary <- dt_summary %>% mutate(average_count = average_AC/n_samples)
-    fwrite(dt_summary, file=paste0(args$out, ".vep_annotations_variant_summary.tsv.gz"), sep="\t", quote=FALSE)
+    # Variant level
+    # just include the most deleterious annotation for a variant
+    dt_summary <- create_MAC_and_MAF_summary(
+        dt_variant_AC, CSQs,
+        paste0(args$out, ".vep_annotations_variant_summary.tsv.gz"))
 }
-
 
 # Add arguments
 parser <- ArgumentParser()
-parser$add_argument("--AC_path", default=NULL, required=TRUE, help="Path to allele count information, output from plink after calling --freqx")
-parser$add_argument("--vep_spliceAI_processed", default=NULL, required=TRUE, help="Path to the processed VEP file with spliceAI information included. This is the 'long' output of brava_create_annot.py")
-parser$add_argument("--out", default=NULL, required=TRUE, help="Output filepath")
+parser$add_argument("--AC_path", default=NULL, required=TRUE,
+    help="Path to allele count information, output from plink --freqx")
+parser$add_argument("--vep_spliceAI_processed", default=NULL, required=TRUE,
+    help=paste0("Path to the processed VEP file with spliceAI information ",
+        "included. This is the 'long' output of brava_create_annot.py"))
+parser$add_argument("--out", default=NULL, required=TRUE,
+    help="Output filepath")
 args <- parser$parse_args()
 
 main(args)
--- run_brava_create_annot.sh
+++ run_brava_create_annot.sh
@@ -15,18 +15,28 @@ echo "------------------------------------------------"
 # Load R module
 module purge
 module load R
+module load BCFtools
 
 spliceai_dir="/well/lindgren/barney/spliceai/out"
 vep_annotations_dir="/well/lindgren/barney/variant-annotation/run-vep/out"
 out_dir="/well/lindgren/UKBIOBANK/dpalmer/annotation_munging"
 annotation_repo_dir="/well/lindgren/UKBIOBANK/dpalmer/variant-annotation"
 brava_create_annot="${annotation_repo_dir}/SAIGE_annotations/scripts/brava_create_annot.py"
+# Taken from this repository
+# https://github.com/BRaVa-genetics/variant-annotation/blob/main/SAIGE_annotations/scripts/brava_create_annot.py
 mkdir -p ${out_dir}
 
 for chr in {{1..22},"X"}
 do
     vep="${vep_annotations_dir}/ukb_wes_450k.qced.chr${chr}.vep_processed.txt"
     spliceai="${spliceai_dir}/ukb_wes_450k.qced.v6.sites_only.${chr}.all.vcf"
+    spliceai_no_chr="${out_dir}/ukb_wes_450k.qced.v6.sites_only.no_chr.${chr}.all.vcf.gz"
     out="${out_dir}/ukb_wes_450k.july.qced.brava_common_rare.v7.chr${chr}.saige.txt"
-    python3 ${brava_create_annot} --vep ${vep} --spliceai ${spliceai} --out_file $out
+    # First, remove the chr from the CHROM column
+    bcftools annotate --rename-chrs <(echo -e "chr${chr}\t${chr}") ${spliceai} -O z -o ${spliceai_no_chr}
+    python3 ${brava_create_annot} --vep ${vep} --spliceai ${spliceai_no_chr} --out_file $out
 done
+
+## Upload the results to the RAP
+# dx mkdir /Duncan/long_annotations/
+# dx upload ${out_dir}/*long.csv.gz --path /Duncan/long_annotations/
